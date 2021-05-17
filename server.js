dc67f3bcf4583b98f07703a05e0067b38f9d8f2d
sandrita <sandra@Sandras-Air.attlocal.net>
2021-05-16 21:36:29 -0700

    Points 1, 2 and 3 and complete

--- server.js
+++ server.js
@@ -4,33 +4,75 @@ projectData = {}
 // Require Express to run server and routes
 
 // Import a function under the name express, cors and body parse which are all of the Node packages we have installed
-const express = require('express'); 
-const cors = require('cors');
-const bodyParser = require('body-parser');
+const express = require('express'); // the librarian - this is how we get access to the server and create it (its a framework or template for node) 
+const cors = require('cors'); //type of middleware - security 
+const bodyParser = require('body-parser');//type of middleware - translator
 
 // Start up an instance of app
 const app = express(); //creating the app variable that will use the information from the express method and will work as instructions
+
+// Establish a connection to the server
 const port = 3000; // we always need a port name. This name is how we will be able to keep calling back
+
 app.listen(port, function() { //here we listen out for the port name and display friendly message to use letting them know if our code is working
   console.log(`Server running on port ${port}`) //message that will pop up if code work and we use a short cut to pull port inforamtion
 });
 
-// respond with "hello world" when a GET request is made to the homepage
-app.get('/Documents', function (req, res){// here 
+// GET request:
+// Retrieves data from the server
+
+// POST request:
+// Submit data to the server
+
+// respond with projectData when a GET request is made to the japanfotos
+app.get('/japanfotos', function (req, res){// here the '/japanfotos' is an example of a path or route. This route or path can be given any name. This is also where the information that we are sending to the server  At this stage it is just being created. Here we are also creating a function within the .get
   res.send(projectData)
 });
 
-const temp = 
-const date = 
-const userResponse = 
+// 
+app.post('/route136', function (req, res) {
 
-// const data = []
-// app.post('/myUrl', addMovie )
+  projectData.fecha = req.body("date")
+  projectData.temperatura = req.body("temperature")
+  projectData.sentimiento = req.body("userMood")
 
-// function addMovie (req, res){
-//   console.log(req.body)
-//   data.push(req.body)
-// }
+})
+
+
+// let a = "Hello"
+// let b = a
+// let c = b
+// let d = c
+// projectData.mood = d // -> Hello
+
+
+
+// let myFantasticMood = "Feeling Inspired"
+// projectData.mood = myFantasticMood
+
+// console.log(myFantasticMood) // -> Feeling inspired
+
+// projectData.mood = "Feeling Inspired"
+
+// console.log(projectData.mood) // -> Feeling inspired
+
+// let a = projectData.mood 
+// console.log(a) // -> Feeling inspired
+
+
+// app.post('/route136', function (req, res) {
+//   // projectData.digitalID  = req.body("email") // The long name is my request name (username)
+//   // projectData.printedName = req.body("name")
+  
+//   projectData.fecha = req.body("date")
+//   projectData.temperatura = req.body("temperature")
+//   projectData.sentimiento = req.body("userMood")
+
+// //   step one: client inputs data on website 
+// //   step two: client sends data inputted to backend server
+// //   step three: data goes through the middle wear tunnel
+// //   step four: data finds assinged route in this case it is called the 'postDoc' where the data inputted by the client will be stored
+// })
 
 
 
--- website/app.js
+++ website/app.js
@@ -1,5 +1,69 @@
 /* Global Variables */
+let baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip='
+let city = document.getElementById('zip');
+let baseKey = '278a4bb0ed3e1dc04317050dfdf8f145';
+
+
+async function farmWaitTime(url, zipCode, key) {
+    // We go and get the zip code that the user has inputted in the html 
+    // Now we can call the weather map API by using the URL of the API, the zipcode of the city, the word &appid which is requried by the API, and the key which represents the credentials
+    const temperatureInCity = await fetch(url+zipCode.value+'&appid='+key)
+};
+
+let ear = document.getElementById('generate')
+
+
+ear.addEventListener("click", function(){
+    farmWaitTime(baseURL, city, baseKey)
+})
+
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let date = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function printMyName() {
+//     return "Tom"
+// }
+
+// let name  = printMyName(); // -> Tom
+
+
+// async function printMyName() {
+//     return "Tom"
+// }
+
+// printMyName().then(alert) // -> Tom
+
+
+
+
+// function printMyName() {
+//     Promise.resolve(console.log("Tom"))
+// }
