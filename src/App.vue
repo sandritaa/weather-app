bbe8a9d6bf5b9691b7e733889f76c0b323924854
Sandrita <sandramilenagarciaa@gmail.com>
2022-01-17 18:59:47 -0800

    First skeleton draft with like button, nasa api and image descriptions

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,47 @@
 <template>
-  <SinglePost v-for="i in [0,1,2,3,4,5,6,7,8,9,10]" :key="i"></SinglePost>
+  <div v-if="loading == false">
+    <SinglePost
+      v-for="singlePost in apiData"
+      :key="singlePost"
+      :postApi="singlePost"
+    ></SinglePost>
+  </div>
+  <div v-else>Loading...</div>
 </template>
 
 <script>
 import SinglePost from "./components/SinglePost.vue";
+import axios from "axios";
 
 export default {
   name: "App",
   components: {
     SinglePost,
   },
+ 
+  data() {
+    return {
+      loading: true,
+      apiData: {},
+    };
+  },
+  created() {
+    // Let's assume the api is loading
+    this.loading = true;
+
+    // Call the API on page load (mounted()) - this is a Promise
+    let myPromise = axios.get(
+      "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=S7Xq2VPauHACIBBCLRGPgcegyt5KPmIKH2pmXYfh"
+    );
+
+    // When the Promise is fulfilled, store the data in a object that can be used outside the function (apiData)
+    myPromise.then((response) => {
+      this.apiData = response.data.photos;
+      this.loading = false;
+    });
+  },
 };
+
 </script>
 
 <style>
--- src/components/SinglePost.vue
+++ src/components/SinglePost.vue
@@ -1,18 +1,74 @@
 <template>
-  <h1>single post</h1>
+  <!-- <h1>single post</h1> -->
 
-  <div>
-    <img src="" alt="" />
-    <h2>image description</h2>
-    <h3>date</h3>
-    <button>like</button>
-    <button>unlike</button>
-  </div>
+  <div class="container">
+    
+    <img v-bind:src="postApi.img_src" alt="" />
   
+    <h2>
+    
+      Rover name: {{ postApi.rover.name }} 
+      <br>
+      Rover landing date:{{ postApi.rover.landing_date }} 
+      <br>
+      Rover launch date:
+      {{ postApi.rover.launch_date }} 
+      <br>
+      Rover status:
+      {{ postApi.rover.status }} 
+      <br>
+      Camera: {{ postApi.camera.full_name }}
+    </h2>
+    <h3>Earth Date: {{ postApi.earth_date }}</h3>
+
+    <!-- <p>{{ like }}</p> -->
+    <button v-if="like == true" style="background-color:green" @click="like=!like">unlike</button>
+    <button v-else  @click="like = !like">like</button>
+
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      like: false,
+    };
+  },
+  props: {
+    postApi: {
+      type: Object,
+    },
+  },
+};
 </script>
 
-<style></style>
+<style>
+.container
+ {
+     position: center;
+  background-color: lightgrey;
+  width: 600px;
+  border: 15px solid green;
+  padding: 50px;
+  margin: auto;
+    box-shadow: 1px 1px #888888;
+
+  /* box-sizing: border-box; */
+  /* border: 0.5px solid #ffffff;
+  padding: 5px;
+  box-shadow: 1px 1px #888888; */
+ 
+  /* height: 100px; */
+  }
+
+img {
+  max-width: inherit;
+  max-height: inherit;
+  height: inherit;
+  width: inherit;
+  object-fit: cover;
+}
+
+  
+</style>
