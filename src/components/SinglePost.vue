d3e545a289d42e02db8053293bdcf428a60bfe1b
Sandrita <sandramilenagarciaa@gmail.com>
2022-01-18 10:26:44 -0800

    css font improvement

--- src/components/SinglePost.vue
+++ src/components/SinglePost.vue
@@ -2,29 +2,30 @@
   <!-- <h1>single post</h1> -->
 
   <div class="container">
-    
     <img v-bind:src="postApi.img_src" alt="" />
-  
+
     <h2>
       Rover status:
-      {{ postApi.rover.status }} 
-      <br>
-      Rover name: {{ postApi.rover.name }} 
-      <br>
-       Rover landing date:{{ postApi.rover.landing_date }} 
-      <br>
-      Rover launch date:
-      {{ postApi.rover.launch_date }} 
-      <br>
-    
+      {{ postApi.rover.status }}
+      <br />
+      Rover name: {{ postApi.rover.name }}
+      <br />
+      Earth Date: {{ postApi.earth_date }}
+      <br />
+      Rover launch date: {{ postApi.rover.launch_date }}
+      <br />
+      Rover landing date: {{ postApi.rover.landing_date }}
+      <br />
       Camera: {{ postApi.camera.full_name }}
     </h2>
-    <h3>Earth Date: {{ postApi.earth_date }}</h3>
 
     <!-- <p>{{ like }}</p> -->
-    <button v-if="like == true" style="background-color:green" @click="like=!like">unlike</button>
-    <button v-else  @click="like = !like">like</button>
-
+    <button
+      v-if="like == true"
+      style="background-color: green"
+      @click="like = !like">unlike
+    </button>
+    <button v-else @click="like = !like">like</button>
   </div>
 </template>
 
@@ -44,16 +45,14 @@ export default {
 </script>
 
 <style>
-.container
- {
+.container {
   position: center;
   background-color: rgb(219, 219, 219);
   width: 600px;
   border: 15px solid rgb(241, 241, 241);
   padding: 50px;
   margin: auto;
-
-  }
+}
 
 img {
   max-width: inherit;
@@ -63,5 +62,7 @@ img {
   object-fit: cover;
 }
 
-  
+h2 {
+  font-family: "Assistant", sans-serif;
+}
 </style>
